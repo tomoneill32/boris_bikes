216d8f515e5e665cad3055b6526d494e50f5c936
Russell <russellmorbey@hotmail.co.uk>
2022-04-20 17:47:29 +0100

    Refactor stage 9.

--- spec/DockingStation_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'DockingStation'
-
-describe DockingStation do
-  it 'responds to the method release_bike' do
-    expect(subject).to respond_to :release_bike
-  end
-end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,5 @@
+require 'docking_station'
+
+describe DockingStation do
+  it { expect(subject).to respond_to :release_bike }
+end
