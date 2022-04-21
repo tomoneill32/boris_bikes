3fbca7b5f8675b08d463a1a1029220d4fe25375a
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-04-21 16:07:28 +0100

    onto step 12, pair swap

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,27 +2,25 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it { expect(subject).to respond_to :release_bike }
+  it { expect(subject).to respond_to :release_bike } #unit test
 
-  it 'Checking to see if a bike from the docking station works' do 
+  it 'Checking to see if a bike from the docking station works' do #feature test
     station = DockingStation.new
-  
     bike = station.release_bike
-
-
     expect(bike.working?).to eq(true)
- 
   end
 
-  it { expect(subject).to respond_to(:dock).with(1).argument }
+  it { expect(subject).to respond_to(:dock).with(1).argument } #unit test
 
-  it 'Can dock a bike at a docking station' do
+  it 'Can dock a bike at a docking station' do #feature test
     bike = Bike.new
     station = DockingStation.new
     expect(station.dock(bike)).to eq(bike)
   end
 
-  it 'Can see a bike that has been docked' do
+  it { is_expected.to respond_to(:stored_bike) } #unit test
+
+  it 'Can see a bike that has been docked' do # feature test
     station = DockingStation.new
     bike = Bike.new
     station.dock(bike)
