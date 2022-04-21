69815f310f92f6568cc6243e16db37e13be8856d
Tom O'Neill <tomoneill32@gmail.com>
2022-04-21 15:13:48 +0100

    Pair swap step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,4 +4,8 @@ class DockingStation
   def release_bike
     return Bike.new
   end
+
+  def dock(bike)
+    
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,26 +4,26 @@ describe DockingStation do
 
   it { expect(subject).to respond_to :release_bike }
 
-  #Arrange
-
-  # create a docking station
   it 'Checking to see if a bike from the docking station works' do 
     station = DockingStation.new
-
-    #Act
-   # release a bike from the docking station
   
     bike = station.release_bike
 
-    #Assert
-    # check to see whether this bike works
 
     expect(bike.working?).to eq(true)
+ 
   end
-  
+
+  it { expect(subject).to respond_to :dock }
+  #unit test dock works on DockingStation
+
+  it 'Can dock a bike at a docking station' do
+    #Arrange
+    bike = Bike.new
+    station = DockingStation.new
+
+    #Assert
+    expect(station.dock(bike)).to eq(bike)
+  end
 
 end
-
-# Add a test to your DockingStation specification that 
-# a) gets a bike, and then 
-# b) expects the bike to be working
