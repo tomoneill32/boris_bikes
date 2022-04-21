a07d05d5998a82c32a7e36e7f2533038dcf87d39
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-04-21 14:41:19 +0100

    pair swap stage 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,27 @@
 require 'docking_station'
 
 describe DockingStation do
+
   it { expect(subject).to respond_to :release_bike }
+
+  #Arrange
+
+  # create a docking station
+
+  station = DockingStation.new
+
+  #Act
+
+  # release a bike from the docking station
+  bike = station.release_bike
+
+  #Assert
+
+  # check to see whether this bike works
+
+
 end
+
+# Add a test to your DockingStation specification that 
+# a) gets a bike, and then 
+# b) expects the bike to be working
